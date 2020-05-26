42833ef2d9c5e871128c824c7d6ca786200a1c69
Bal <bal@bal.techies>
2020-05-26 15:52:49 +0530

    project setup and refractor index js and component

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import SearchBar from './SearchBar'
+
+const App =()=>{
+        // return <div>Hello World</div>;  
+        return (<SearchBar/>);
+    
+}
+export default App;
