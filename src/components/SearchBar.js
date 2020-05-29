35185dcb9762f5a981e76022a328e95204145249
gaurav-techies <gauravkamboj13121992@gmail.com>
2020-05-29 10:39:52 +0530

    binding events

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,42 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
-    constructor(props){
-            super(props);
-        this.onFormSubmit.bind(this);
-    }
+  
     state ={term:'HI there'}
     onInputChange(event){
         console.log(event.target.value)
     }
-    onFormSubmit(event){
-        event.preventDefault();
+    onFormSubmit(e){
+        
+      //This will come undefined because this is similar to :- *1*
+        e.preventDefault(); 
+        
         console.log(this.state.term)
+      
     }
+/*  *1*
+class Car{
+    setDriverSound(sound){
+        this.sound=sound;
+    }
+    drive(){
+        return this.sound
+    }
+}
+
+const car= new Car();
+const drive=car.drive;    //car.drivr is reference to function not returned value and Now drive is a function 
+drive();  //now this will result in  "this is undefined" because dive have nothing like this 
+//we have to see on left from calling to know what will be the value of this will come 
+//In handlers also it come undefined because  onSubmit={this.onFormSubmit} we are putting function into onFormSubmit and 
+//this is undefined in onform submit 
+
+*/
 
     render(){
         return (
             <div className="ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
+                <form onSubmit={(e)=>this.onFormSubmit(e)} className="ui form">
                     <div className="field">
                         <label>Search Bar</label>
                     {/* These are different method of handling events  */}
