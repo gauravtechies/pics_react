c1deaeab58c44ea29d643ddaec8aec872210ba9f
gaurav-techies <gauravkamboj13121992@gmail.com>
2020-06-05 12:28:39 +0530

    keys and props

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const ImageList = props =>{
     console.log(props.images);
-    return <div>Images</div>
+    const image = props.images.map((img)=>{
+        return <img alt={img.description} key={img.id} src={img.urls.regular}></img>
+    })
+    return <div>{image}</div>
 
 }
 export default ImageList;
